a53b1507298876e56bc5151f7788b07f8cf90dfe
Naoki <sh0120hp@gmail.com>
2025-03-22 21:30:18 +0900

    students、studentcourses統合メソッド追加コミット remark、isDeletedテーブル追加コミット

--- src/main/java/reisetech/student/management/controller/StudentController.java
+++ src/main/java/reisetech/student/management/controller/StudentController.java
@@ -4,27 +4,34 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reisetech.student.management.controller.converter.StudentConverter;
 import reisetech.student.management.data.Student;
 import reisetech.student.management.data.StudentCourses;
+import reisetech.student.management.domain.StudentDetail;
 import reisetech.student.management.service.StudentService;
 
 @RestController
 public class StudentController {
 
     private StudentService service;
+    private StudentConverter converter;
 
     @Autowired
-    public StudentController (StudentService service){
-        this.service=service;
+    public StudentController(StudentService service, StudentConverter converter) {
+        this.service = service;
+        this.converter = converter;
     }
 
     @GetMapping("/studentList")
-    public List<Student> getStudentList() {
-        return service.searchStudentList();
+    public List<StudentDetail> getStudentList() {
+        List<Student> students = service.searchStudentList();
+        List<StudentCourses> studentCourses = service.searchStudentCoursesList();
+
+        return converter.getStudentDetails(students, studentCourses);
     }
 
     @GetMapping("/studentCourseList")
-    public List<StudentCourses> getStudnetCourseList(){
+    public List<StudentCourses> getStudnetCourseList() {
         return service.searchStudentCoursesList();
     }
 }
