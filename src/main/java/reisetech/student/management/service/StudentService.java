af0a2533d49f93e58ec25e22d01738027d73720e
Naoki <sh0120hp@gmail.com>
2025-04-13 17:35:27 +0900

    update機能追加コミット

--- src/main/java/reisetech/student/management/service/StudentService.java
+++ src/main/java/reisetech/student/management/service/StudentService.java
@@ -1,10 +1,13 @@
 package reisetech.student.management.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reisetech.student.management.data.Student;
 import reisetech.student.management.data.StudentCourses;
+import reisetech.student.management.domain.StudentDetail;
 import reisetech.student.management.repository.StudentRepository;
 
 @Service
@@ -22,12 +25,38 @@ public class StudentService {
     }
 
     public List<StudentCourses> searchStudentCoursesList() {
-        return repository.searchCourse();
+        return repository.searchCoursesList();
     }
 
-    public int insertStudent(Student student) {
-        return repository.registerStudent(student);
+    @Transactional
+    public void insertStudent(StudentDetail studentDetail) {
+        repository.registerStudent(studentDetail.getStudent());
+        for (StudentCourses studentCourses : studentDetail.getStudentCourses()) {
+            studentCourses.setStudentId(studentDetail.getStudent().getId());
+            studentCourses.setStartDate(LocalDateTime.now());
+            studentCourses.setEndDate(LocalDateTime.now().plusYears(1));
+            repository.registerStudentsCourses(studentCourses);
+        }
     }
 
+    public StudentDetail findStudent(String id) {
+        Student student = repository.findStudentById(id);
+        List<StudentCourses> studentCourses = repository.findStudentCoursesById(
+                student.getId());
+        StudentDetail studentDetail = new StudentDetail();
+        studentDetail.setStudent(student);
+        studentDetail.setStudentCourses(studentCourses);
+        return studentDetail;
+    }
+
+    @Transactional
+    public void updateStudentDetail(StudentDetail studentDetail) {
+        repository.updateStudent(studentDetail.getStudent());
+        for (StudentCourses studentCourses : studentDetail.getStudentCourses()) {
+            repository.updateStudentCourses(studentCourses);
+        }
+    }
 }
+
+
 
